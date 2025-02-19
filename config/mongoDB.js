4715326a0d5d50d497bebb9534ac17a3f902a869
Mohiuddin10 <sj.ctg6737@gmail.com>
2025-02-20 01:30:57 +0600

    cloud connection done

--- config/mongoDB.js
+++ config/mongoDB.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 
 const connectMongoDB = async () => {
     try {
-        await mongosh.connect(process.env.MONGO_URL)
+        await mongosh.connect(`mongodb+srv://${process.env.User}:${process.env.Mongo_pass}@cluster0.1r4sa.mongodb.net/mmtrading?retryWrites=true&w=majority&appName=Cluster0/`)
         console.log("mongoDB connected");
     } catch (error) {
         console.log(error.message);
